82399390026af986762e8475d0d25ebfd6a0d713
Nguyen Tuan Hiep <hiep454546@gmail.com>
2023-05-15 12:19:45 +0700

    :sparkles: add footer

---
+++ client/src/components/constants.jsx
@@ -0,0 +1,78 @@
+// import { BsQuestionCircleFill, BsStars } from 'react-icons/bs';
+// import { MdOutlineAccessTimeFilled, MdSmokeFree } from 'react-icons/md';
+// import { TbBallonOff } from 'react-icons/tb';
+
+export const thingsToKnow = [
+  {
+    title: 'House rules',
+    content: [
+      { name: 'Check-in: After 3:00 PM' },
+      { name: 'Checkout: 11:00 AM', icon:  ""},
+      { name: 'No smoking', icon:  ""},
+      { name: 'No parties or events', icon:  ""},
+    ],
+  },
+  {
+    title: 'Health & safety',
+    content: [
+      { name: "Airbnb's COVID-19 safety practices apply", icon: ""},
+      {
+        name: 'Carbon monoxide alarm not reported',
+        icon: "",
+      },
+      { name: 'Smoke alarm not reported', icon:  ""},
+    ],
+  },
+  {
+    title: 'Cancellation policy',
+    content: [
+      { name: 'Free cancellation before Oct 22.' },
+      {
+        name: 'Review the Hosts full cancellation policy which applies even if you cancel for illness or disruptions caused by COVID-19.',
+      },
+    ],
+  },
+];
+
+export const footer = [
+  {
+    title: 'Support',
+    content: [
+      'Help Center',
+      'AirCover',
+      'Safety information',
+      'Supporting people with disabilities',
+      'Cancellation options',
+      'Report a neighborhood concern',
+    ],
+  },
+  {
+    title: 'Community',
+    content: [
+      'Airbnb.org: disaster relief housing',
+      'Support Afghan refugees',
+      'Combating discrimination',
+    ],
+  },
+  {
+    title: 'Hosting',
+    content: [
+      'Try hosting',
+      'AirCover for Hosts',
+      'Explore hosting resources',
+      'Visit our community forum',
+      'How to host responsibly',
+    ],
+  },
+  {
+    title: 'Airbnb',
+    content: [
+      'Newsroom',
+      'Learn about new features',
+      'Letter from our founders',
+      'Careers',
+      'Investors',
+      'Gift cards',
+    ],
+  },
+];
