d0c64e17c1a141f61f4ffd977213cde6c220b60f
Nguyen Tuan Hiep <hiep454546@gmail.com>
2023-05-15 11:29:03 +0700

    fix layout

--- client/src/components/BookingWidget.jsx
+++ client/src/components/BookingWidget.jsx
@@ -2,11 +2,16 @@ import React, { useContext, useEffect, useState } from 'react';
 import { differenceInDays } from 'date-fns';
 import axios from 'axios';
 import { Navigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { UserContext } from '../providers/UserProvider';
 
 const BookingWidget = ({ place }) => {
-  const [checkIn, setCheckIn] = useState('');
-  const [checkOut, setCheckOut] = useState('');
+  const [checkIn, setCheckIn] = useState(null);
+  const [checkOut, setCheckOut] = useState(null);
   const [noOfGuests, setNoOfGuests] = useState(1);
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
@@ -49,50 +54,67 @@ const BookingWidget = ({ place }) => {
       <div className="text-xl text-center">
         Price: ₹{place.price} / per night
       </div>
-      <div className="border rounded-2xl mt-4">
-        <div className="flex">
-          <div className="py-3 px-4">
-            <label>Check In: </label>
-            <input
-              type="date"
-              value={checkIn}
-              onChange={(e) => setCheckIn(e.target.value)}
-            />
-          </div>
-          <div className="py-3 px-4 border-l">
-            <label>Check out: </label>
-            <input
-              type="date"
-              value={checkOut}
-              onChange={(e) => setCheckOut(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="py-3 px-4 border-t">
-          <label>Number of guests: </label>
-          <input
+      <Stack spacing={2} mt={2} px={1}>
+        <LocalizationProvider dateAdapter={AdapterDateFns} className="px-2">
+          <DatePicker
+            label="Check In"
+            value={checkIn}
+            onChange={(newValue) => {
+              setCheckIn(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+            format="dd/MM/yyyy"
+            minDate={new Date()}
+          />
+          <DatePicker
+            label="Check out"
+            value={checkOut}
+            onChange={(newValue) => {
+              setCheckOut(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+            format="dd/MM/yyyy"
+            minDate={checkIn ? new Date(checkIn) : new Date()}
+          />
+        </LocalizationProvider>
+        <div className="pt-3 pb-1 border-t">
+          <TextField
+            label="Number or guest"
+            variant="outlined"
             type="number"
-            value={noOfGuests}
-            onChange={(e) => setNoOfGuests(e.target.value)}
+            style={{ width: '100%', padding: '0px' }}
+            InputProps={{
+              inputProps: {
+                min: 0, // Set min value to 0 to prevent negative numbers
+              },
+            }}
           />
         </div>
         {numberOfNights > 0 && (
-          <div className="py-3 px-4 border-t">
-            <label>Your full name: </label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label>Phone number: </label>
-            <input
-              type="tel"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
+          <div>
+            <div className="pt-4 pb-4 border-t">
+              <TextField
+                label="Your full name"
+                variant="outlined"
+                type="text"
+                style={{ width: '100%', padding: '0px' }}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <TextField
+                label="Phone number"
+                variant="outlined"
+                type="tel"
+                style={{ width: '100%', padding: '0px' }}
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
           </div>
         )}
-      </div>
+      </Stack>
       <button onClick={handleBooking} className="primary mt-4">
         Book this place
         {numberOfNights > 0 && <span> ₹{numberOfNights * place.price}</span>}
