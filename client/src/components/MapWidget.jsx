147df72e5a07840a1ba24656ded2ba5b90ecc563
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2023-05-18 21:00:45 +0700

    Complete input using map on PlacesFormPage.jsx

--- client/src/components/MapWidget.jsx
+++ client/src/components/MapWidget.jsx
@@ -3,20 +3,10 @@ import GoogleMapReact from 'google-map-react';
 import axios from 'axios';
 import { MapContext } from '../providers/MapProvider.jsx';
 
-const MapWidget = (id) => {
-  const {lat, setLat} = useContext(MapContext);
-  const {lng, setLng} = useContext(MapContext);
+const MapWidget = () => {
   const {address, setAddress} = useContext(MapContext);
-  // if (id) {console.log(id)}
   const handleMapClick = async (event) => {
-
-    const lat = event.lat;
-    const lng = event.lng;
-    setLat(lat);
-    setLng(lng);
-    // Use lat and lng to get address
-    // ...
-    const response = await axios.get('/reverse-geocode', {params: {lat: lat, lng: lng}});
+    const response = await axios.get('/reverse-geocode', {params: {lat: event.lat, lng: event.lng}});
     if (response.status === 200) {
       setAddress(response.data.address);
     }
@@ -28,13 +18,9 @@ const MapWidget = (id) => {
       <GoogleMapReact
         bootstrapURLKeys={{ key: 'AIzaSyDNI_ZWPqvdS6r6gPVO50I4TlYkfkZdXh8' }}
         defaultCenter={{ lat: 21.09, lng: 105.80 }}
-        defaultZoom={9}
+        defaultZoom={10}
         onClick={handleMapClick}
       ></GoogleMapReact>
-      <div>
-        Latitude: {lat}, Longitude: {lng}
-      </div>
-      <div>Address: {address}</div>
     </div>
   );
 };
