404c73b8673afd6f80ba4db62a36f903bed36ab6
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2023-05-18 22:42:31 +0700

    Testing click bookmark button

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { HeartIcon } from '@heroicons/react/solid';
 
 const Image = ({ src, ...rest }) => {
+  function handleClick(event) {
+    event.preventDefault();
+    console.log('Button clicked');
+  }
+
   return (
     <div>
-      <img src={src} {...rest} alt={''} className="rounded-xl" />
+      <img src={src} {...rest} alt={''} className="rounded-xl mb-2" />
       <button>
         <HeartIcon
-          className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2
-        cursor-pointer absolute top-4 right-4  hover:bg-gray-300 duration-200 ease-out"
+          className="z-0 hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2
+                  cursor-pointer absolute top-4 right-4  hover:bg-gray-300 duration-200 ease-out"
+          onClick={(e) => {handleClick(e)}}
         />
       </button>
     </div>
