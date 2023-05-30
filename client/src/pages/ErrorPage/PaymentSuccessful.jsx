3121453a63836a0e5fc3351d387bc3c389e9fb0e
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2023-05-30 11:06:51 +0700

    Fix send mail two times

--- client/src/pages/ErrorPage/PaymentSuccessful.jsx
+++ client/src/pages/ErrorPage/PaymentSuccessful.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { getItemFromLocalStorage } from '../../utils/index.js';
 import emailjs from '@emailjs/browser';
 import { differenceInCalendarDays } from 'date-fns';
 
 function PaymentSuccessful() {
-  const { id } = useParams();
   const [bookings, setBookings] = useState([]);
+  const [canSendMail, setCanSendMail] = useState(false);
+  const [mailSended, setMailSended] = useState(false);
   const navigate = useNavigate();
   const token = getItemFromLocalStorage('token');
 
@@ -16,71 +17,80 @@ function PaymentSuccessful() {
   };
 
   useEffect(() => {
-    // if (id) {
-      const getBookings = async () => {
-        try {
-          const { data } = await axios.get('/bookings', {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
+      if (!canSendMail) {
+        const getBookings = async () => {
+          try {
+            const { data } = await axios.get('/bookings', {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
 
-          // Create an array of all unique user IDs from the bookings
-          const userIds = Array.from(
-            new Set(data.map((booking) => booking.user))
-          );
+            // Create an array of all unique user IDs from the bookings
+            const userIds = Array.from(
+              new Set(data.map((booking) => booking.user))
+            );
 
-          // Fetch the user data for each unique user ID
-          const { data: users } = await axios.get(
-            `/user?id=${userIds.join(',')}`
-          );
+            // Fetch the user data for each unique user ID
+            const { data: users } = await axios.get(
+              `/user?id=${userIds.join(',')}`
+            );
 
-          // Replace the 'user' field with the corresponding user object
-          const populatedData = data.map((booking) => ({
-            ...booking,
-            user: users.find((user) => user._id === booking.user),
-          }));
-          setBookings(populatedData);
-        } catch (error) {
-          toast.error('Failed to fetch bookings');
+            // Replace the 'user' field with the corresponding user object
+            const populatedData = data.map((booking) => ({
+              ...booking,
+              user: users.find((user) => user._id === booking.user),
+            }));
+            setBookings(populatedData);
+          } catch (error) {
+            toast.error('Failed to fetch bookings');
+          }
+        };
+
+        getBookings().then(() => {setCanSendMail(true);});
+
+      }
+      else {
+        if (bookings.length > 0 && !mailSended) {
+          setMailSended(true);
+          // setEmailSent(true);
+          const sendEmail = () => {
+            emailjs
+              .send(
+                'service_2f72jif',
+                'template_ygd8z4b',
+                {
+                  to_name: bookings[0].name,
+                  to_email: bookings[0].user.email,
+                  place: bookings[bookings.length - 1].place.title,
+                  price: bookings[bookings.length - 1].price,
+                  booking_id: bookings[bookings.length - 1]._id,
+                  total_nights: differenceInCalendarDays(
+                    new Date(bookings[bookings.length - 1].checkOut),
+                    new Date(bookings[bookings.length - 1].checkIn)
+                  ),
+                  address: bookings[bookings.length - 1].place.address,
+                },
+                'A1Me166m6TwcmksZP'
+              )
+              .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+              })
+              .catch((error) => {
+                console.error('FAILED...', error);
+              });
+          };
+          sendEmail();
+          console.log('sent email');
         }
-      };
-
-      getBookings();
+      }
     // }
-  }, []);
-
-  useEffect(() => {
-    if (bookings.length > 0) {
-      const sendEmail = () => {
-        emailjs
-          .send(
-            'service_2f72jif',
-            'template_ygd8z4b',
-            {
-              to_name: bookings[0].name,
-              to_email: bookings[0].user.email,
-              place: bookings[bookings.length - 1].place.title,
-              price: bookings[bookings.length - 1].price,
-              booking_id: bookings[bookings.length - 1]._id,
-              total_nights: differenceInCalendarDays(
-                new Date(bookings[bookings.length - 1].checkOut),
-                new Date(bookings[bookings.length - 1].checkIn)
-              ),
-              address: bookings[bookings.length - 1].place.address,
-            },
-            'A1Me166m6TwcmksZP'
-          )
-          .then((response) => {
-            console.log('SUCCESS!', response.status, response.text);
-          })
-          .catch((error) => {
-            console.error('FAILED...', error);
-          });
-      };
-      sendEmail();
-    }
   }, [bookings]);
+
+  // useEffect(() => {
+  //
+  //
+  // }, [bookings]);
 
   return (
     <>
