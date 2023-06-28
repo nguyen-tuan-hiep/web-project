a51bf5d207623181208e1618b30c8e382216d7cd
Nguyen Tuan Hiep <hiep454546@gmail.com>
2023-06-28 16:00:23 +0700

    fix version confliction

--- client/src/components/storages/MapWithDirections.jsx
+++ client/src/components/storages/MapWithDirections.jsx
@@ -1,27 +1,29 @@
 import React, { useEffect } from 'react';
-import mapboxgl from 'mapbox-gl';
-import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+// import mapboxgl from 'mapbox-gl';
 
 const MapWithDirections = ({ latitude, longitude }) => {
   useEffect(() => {
     mapboxgl.accessToken =
       'pk.eyJ1IjoiaGllcG50MiIsImEiOiJjbGpmNjFkbDUweGZmM2dzY2k2MWMzYmR2In0.NOF_FyU01LeUl5aWXh5GhA';
 
-    const directions = new MapboxDirections({
-      accessToken: mapboxgl.accessToken,
-    });
-
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [longitude, latitude],
-      zoom: 13,
+      zoom: 15,
     });
 
-    map.addControl(directions, 'top-left');
+    map.addControl(
+      new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+      }),
+      'top-left'
+    );
+
     const marker1 = new mapboxgl.Marker()
       .setLngLat([longitude, latitude])
       .addTo(map);
+
     return () => {
       map.remove();
     };
